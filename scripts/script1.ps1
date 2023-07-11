b3f3e70bd90c575d18aa842e334bbb147dbcd554
iamarjotsingh <amarjotsingh5555@gmail.com>
2023-07-11 19:04:49 +0530

    adding script file

---
+++ scripts/script1.ps1
@@ -0,0 +1,23 @@
+$Result = @()
+#Get all Resource groups
+$AllRGs = Get-AzureRMResourceGroup
+$TotalGroups = $AllRGs.Count
+$i = 1 
+ForEach ($RG in $AllRGs) {
+Write-Progress -Activity "Processing $($RG.ResourceGroupName)" -Status "$i out of $TotalGroups Resource groups completed"
+#Fetch virtual machines in the group
+$VMList = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName -Status
+  
+ForEach ($VM in $VMList) {
+$Result += New-Object PSObject -property $([ordered]@{ 
+VMName = $VM.Name
+VMStatus  = $VM.PowerState
+ProvisioningState = $VM.ProvisioningState
+ResourceGroup = $VM.ResourceGroupName
+Location = $VM.Location
+})
+}
+$i++
+}
+#$Result | FT
+$Result | Export-CSV "C:\AllAzureVMStatus.CSV" -NoTypeInformation -Encoding UTF8

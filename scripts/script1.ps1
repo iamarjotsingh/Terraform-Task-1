a4cd93bc0cea2ee63b78f264d5f6cb03743b6db7
iamarjotsingh <amarjotsingh5555@gmail.com>
2023-07-12 18:53:43 +0530

    changes done

--- scripts/script1.ps1
+++ scripts/script1.ps1
@@ -1,25 +1,26 @@
 
 
-$Result = @()
-#Get all Resource groups
-$AllRGs = Get-AzResourceGroup
-$TotalGroups = $AllRGs.Count
-$i = 1 
-ForEach ($RG in $AllRGs) {
-Write-Progress -Activity "Processing $($RG.ResourceGroupName)" -Status "$i out of $TotalGroups Resource groups completed"
-#Fetch virtual machines in the group
-$VMList = Get-AzVM -ResourceGroupName $RG.ResourceGroupName -Status
+# $Result = @()
+# #Get all Resource groups
+# $AllRGs = Get-AzResourceGroup
+# $TotalGroups = $AllRGs.Count
+# $i = 1 
+# ForEach ($RG in $AllRGs) {
+# Write-Progress -Activity "Processing $($RG.ResourceGroupName)" -Status "$i out of $TotalGroups Resource groups completed"
+# #Fetch virtual machines in the group
+# $VMList = Get-AzVM -ResourceGroupName $RG.ResourceGroupName -Status
   
-ForEach ($VM in $VMList) {
-$Result += New-Object PSObject -property $([ordered]@{ 
-VMName = $VM.Name
-VMStatus  = $VM.PowerState
-ProvisioningState = $VM.ProvisioningState
-ResourceGroup = $VM.ResourceGroupName
-Location = $VM.Location
-})
-}
-$i++
-}
-$Result | FT # alias Format Table
-$Result | Export-CSV -Path ".\AllAzureVMStatus.CSV" -NoTypeInformation -Encoding UTF8
+# ForEach ($VM in $VMList) {
+# $Result += New-Object PSObject -property $([ordered]@{ 
+# VMName = $VM.Name
+# VMStatus  = $VM.PowerState
+# ProvisioningState = $VM.ProvisioningState
+# ResourceGroup = $VM.ResourceGroupName
+# Location = $VM.Location
+# })
+# }
+# $i++
+# }
+# $Result | FT # alias Format Table
+
+# $Result | Export-CSV -Path "C:/AllAzureVMStatus.CSV" -NoTypeInformation -Encoding UTF8

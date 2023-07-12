91033f530a166544c7bd8515eb82ead76df8198f
iamarjotsingh <amarjotsingh5555@gmail.com>
2023-07-12 10:54:40 +0530

    changes done

--- scripts/script1.ps1
+++ scripts/script1.ps1
@@ -2,13 +2,13 @@
 
 $Result = @()
 #Get all Resource groups
-$AllRGs = Get-AzureRMResourceGroup
+$AllRGs = Get-AzResourceGroup
 $TotalGroups = $AllRGs.Count
 $i = 1 
 ForEach ($RG in $AllRGs) {
 Write-Progress -Activity "Processing $($RG.ResourceGroupName)" -Status "$i out of $TotalGroups Resource groups completed"
 #Fetch virtual machines in the group
-$VMList = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName -Status
+$VMList = Get-AzVM -ResourceGroupName $RG.ResourceGroupName -Status
   
 ForEach ($VM in $VMList) {
 $Result += New-Object PSObject -property $([ordered]@{ 

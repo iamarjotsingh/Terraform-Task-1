b16ab7303e862121339df95c0632dc3cb97de20a
iamarjotsingh <amarjotsingh5555@gmail.com>
2023-07-14 22:49:56 +0530

    adding files

--- scripts/script2.ps1
+++ scripts/script2.ps1
@@ -1,31 +1,55 @@
+Get-ExecutionPolicy
 
-# Variables
-$resourceGroupName = "terrform-amar-rg1"
-$vmName = "tf-vm"
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
 
-# Get the VM object
-$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+Install-Module posh-git -Scope CurrentUser -Force
 
-# Check if Docker is already installed
-if ($vm.Extensions | Where-Object { $_.Publisher -eq "Microsoft.Azure.Extensions" -and $_.VirtualMachineExtensionType -eq "DockerExtension" }) {
-    Write-Host "Docker is already installed on the VM."
-} else {
-    # Add the Docker extension to the VM
-    Set-AzVMExtension -ResourceGroupName $resourceGroupName -VMName $vmName `
-        -Name "DockerExtension" -Publisher "Microsoft.Azure.Extensions" `
-        -Type "DockerExtension" -TypeHandlerVersion "1.4" `
-        -SettingString '{"docker": {"port": "2375"}}'
+Import-Module posh-git
 
-    # Update the VM
-    Update-AzVM -ResourceGroupName $resourceGroupName -VM $vm
+Add-PoshGitToProfile -AllHosts​​​​​​​
 
-    # Wait for the VM to be updated
-    $vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
-    while ($vm.Extensions | Where-Object { $_.ProvisioningState -eq "Creating" -or $_.ProvisioningState -eq "Updating"}) {
-        Write-Host "Waiting for VM update..."
-        Start-Sleep -Seconds 30
-        $vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
-    }
 
-    Write-Host "Docker has been installed on the VM."
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Variables
+# $resourceGroupName = "terrform-amar-rg1"
+# $vmName = "tf-vm"
+
+# # Get the VM object
+# $vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+
+# # Check if Docker is already installed
+# if ($vm.Extensions | Where-Object { $_.Publisher -eq "Microsoft.Azure.Extensions" -and $_.VirtualMachineExtensionType -eq "DockerExtension" }) {
+#     Write-Host "Docker is already installed on the VM."
+# } else {
+#     # Add the Docker extension to the VM
+#     Set-AzVMExtension -ResourceGroupName $resourceGroupName -VMName $vmName `
+#         -Name "DockerExtension" -Publisher "Microsoft.Azure.Extensions" `
+#         -Type "DockerExtension" -TypeHandlerVersion "1.4" `
+#         -SettingString '{"docker": {"port": "2375"}}'
+
+#     # Update the VM
+#     Update-AzVM -ResourceGroupName $resourceGroupName -VM $vm
+
+#     # Wait for the VM to be updated
+#     $vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+#     while ($vm.Extensions | Where-Object { $_.ProvisioningState -eq "Creating" -or $_.ProvisioningState -eq "Updating"}) {
+#         Write-Host "Waiting for VM update..."
+#         Start-Sleep -Seconds 30
+#         $vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+#     }
+
+#     Write-Host "Docker has been installed on the VM."
+# }
